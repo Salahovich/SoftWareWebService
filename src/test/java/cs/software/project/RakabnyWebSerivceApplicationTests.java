be98d36fb7a4793717ebdc6ce424453bdbc8015d
Salahovich <84870808+Salahovich@users.noreply.github.com>
2022-01-01 10:51:22 +0200

    Create RakabnyWebSerivceApplicationTests.java

---
+++ src/test/java/cs/software/project/RakabnyWebSerivceApplicationTests.java
@@ -0,0 +1,13 @@
+package cs.software.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RakabnyWebSerivceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

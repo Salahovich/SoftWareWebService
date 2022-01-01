9df50a397638140453123ed297aa473b579c77fc
Salahovich <84870808+Salahovich@users.noreply.github.com>
2022-01-01 10:52:35 +0200

    Create RakabnyWebSerivceApplication.java

---
+++ src/main/java/cs/software/project/RakabnyWebSerivceApplication.java
@@ -0,0 +1,20 @@
+package cs.software.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+import cs.software.project.service.DataBase;
+import cs.software.project.service.Driver;
+import cs.software.project.service.Client;
+
+@SpringBootApplication
+public class RakabnyWebSerivceApplication {
+
+	public static void main(String[] args) {
+		DataBase.getData().addDriverToSystem(new Driver("ahmed","123456","021323","2001-3-1","@gmail.com","fdsfds","fdsfserew"));
+	/***/	DataBase.getData().addClientToSystem(new Client("mido","123456","021323","2001-3-1","@gmail.com"));
+		SpringApplication.run(RakabnyWebSerivceApplication.class, args);
+	}
+
+}
